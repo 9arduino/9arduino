2a2ad4a72350d0748aac68e3ac8935a8518687d5
9arduino <the9arduino@gmail.com>
2017-03-02 17:45:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,7 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'Hello'
+				'type' => 'text',
+				'text' => $content
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
